13c1860a0e28d7b9e449cadc9164b5c2907014bc
Rayon Hunte <rayon.hunte@gmail.com>
2018-05-22 15:14:41 -0400

    added location message test

--- server/util/message.test.js
+++ server/util/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {genMessage} = require('./message');
+const {genMessage, genLocationMsg} = require('./message');
 
 
 describe('genMessage', ()=>{
@@ -9,5 +9,15 @@ describe('genMessage', ()=>{
         expect(message.from).toBe('rayon');
         expect(message.text).toBe('dont test me');
         expect( typeof message.createAt).toBe('number');
+    });
+});
+
+
+describe('genLocationMsg', ()=>{
+    it('it should generate correct location message', ()=>{
+        const message = genLocationMsg('rayon', 1, 3);
+        expect(message.from).toBe('rayon');
+        expect(typeof message.createAt).toBe('number');
+        expect(message.url).toBe('https://www.google.com/maps?q=1,3');
     });
 });
